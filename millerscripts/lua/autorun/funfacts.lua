81d7c22240b02f523ac9354a33b00d4c5f5b1618
Blair Van Biber <B.Vanbiber@bssdstd.net>
2022-02-11 08:07:02 -0600

    Update funfacts.lua

--- millerscripts/lua/autorun/funfacts.lua
+++ millerscripts/lua/autorun/funfacts.lua
@@ -3,4 +3,6 @@ function facts()
     facts {"A","B","C","D","E","F","G"}
     chat.AddText(facts[math.random(#facts)])
     print(facts[math.random(#facts)])
+end
 timer.create("FunFacts", 1800, 0, facts)
+end
